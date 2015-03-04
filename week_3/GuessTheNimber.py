461421f95a7afef4385c2dfd5d15f19d7461cfe4
Michael Dmytriaha <mdmytiaha@playtika.com.ua>
2015-03-04 11:47:52 +0200

    week3: assignment complete

--- week_3/GuessTheNimber.py
+++ week_3/GuessTheNimber.py
@@ -1,46 +1,72 @@
 # template for "Guess the number" mini-project
 # input will come from buttons and an input field
 # all output for the game will be printed in the console
+import random
+import math
+
+import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
 
-
+secret_number = 0
+new_range = 100
+n = 0
 # helper function to start and restart the game
+
+
 def new_game():
-    # initialize global variables used in your code here
+    global secret_number, n
+    secret_number = random.randint(0, new_range)
+    n = int(math.ceil(math.log(new_range + 1, 2)))
+    print
+    print 'New game. Range is from 0 to ', new_range
+    print 'Number of remaining guesses is ', n
 
-    # remove this when you add your code    
-    pass
 
-
-# define event handlers for control panel
 def range100():
-    # button that changes the range to [0,100) and starts a new game 
-
-    # remove this when you add your code    
-    pass
+    global new_range
+    new_range = 100
+    new_game()
 
 
 def range1000():
-    # button that changes the range to [0,1000) and starts a new game     
-
-    pass
+    global new_range
+    new_range = 1000
+    new_game()
 
 
 def input_guess(guess):
-    # main game logic goes here	
+    global n
+    input_number = int(guess)
+    print
+    print "Guess was ", input_number
 
-    # remove this when you add your code
-    pass
+    n -= 1
 
+    if input_number == secret_number:
+        print 'Correct'
+        new_game()
+        return
+    elif input_number > secret_number:
+        print 'Number of remaining guesses is ', n
+        print 'Higher'
+    elif input_number < secret_number:
+        print 'Number of remaining guesses is ', n
+        print 'Lower'
+
+    if n == 0:
+        print 'You ran out of guesses.  The number was', secret_number
+        new_game()
+        return
 
 # create frame
-
+frame = simplegui.create_frame('GuessTheNumber', 200, 200)
 
 # register event handlers for control elements and start frame
-
+frame.add_button('Range [0-100]', range100, 150)
+frame.add_button('Range [0-1000]', range1000, 150)
+frame.add_input('Enter Number', input_guess, 150)
 
 # call new_game 
 new_game()
 
-
-# always remember to check your completed program against the grading rubric
+frame.start()
