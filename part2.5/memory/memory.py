920b3e860fa9b069780c1a4c30cdd9e74c4503a5
Michael Dmytriaha <mdmytiaha@playtika.com.ua>
2015-04-03 11:44:21 +0300

    week5: memory: game complete

--- part2.5/memory/memory.py
+++ part2.5/memory/memory.py
@@ -7,13 +7,13 @@ import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 EXPOSED = False
 card_indexes = []
 numbers = []
-exposed_cards = []
 cards = {0: True, 1: True, 2: True, 3: True, 4: True, 5: True, 6: True, 7: True, 8: True, 9: True, 10: True, 11: True, 12: True, 13: True, 14: True, 15: True}
 
 # helper function to initialize globals
 def new_game():
-    global numbers, cards, state, card_indexes
+    global numbers, cards, state, card_indexes, counter
     state = 0
+    counter = 0
     card_indexes = []
     numbers = range(0, 8)
     numbers.extend(numbers)
@@ -23,21 +23,20 @@ def new_game():
 
 # define event handlers
 def mouseclick(pos):
-    global cards, state, numbers, card_indexes, exposed_cards
-
+    global cards, state, numbers, card_indexes, counter
     index = pos[0] // 50
     if cards[index] == EXPOSED:
         return
 
     if state == 0:
         state = 1
+        counter += 1
     elif state == 1:
         state = 2
     else:
         state = 1
-        if numbers[card_indexes[0]] == numbers[card_indexes[1]]:
-            exposed_cards.extend(card_indexes)
-        else:
+        counter += 1
+        if numbers[card_indexes[0]] != numbers[card_indexes[1]]:
             cards[card_indexes[0]] = not EXPOSED
             cards[card_indexes[1]] = not EXPOSED
         card_indexes = []
@@ -45,9 +44,9 @@ def mouseclick(pos):
     card_indexes.append(index)
     cards[index] = EXPOSED
 
-# cards are logically 50x100 pixels in size    
+# cards are logically 50x100 pixels in size
 def draw(canvas):
-    global numbers
+    global numbers, counter
     count = 0
     for number in numbers:
         canvas.draw_text(str(number), (count * 50, 80), 100, 'White')
@@ -56,6 +55,7 @@ def draw(canvas):
         if cards[count]:
             canvas.draw_polygon([(x1, 1), (x1, 99), (x2, 99), (x2, 1)], 1, "Black", "Yellow")
         count += 1
+    label.set_text("Turns = " + str(counter))
 
 # create frame and add a button and labels
 frame = simplegui.create_frame("Memory", 800, 100)
