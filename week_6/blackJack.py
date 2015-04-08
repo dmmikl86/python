4f78867cfc9a4e7222341fef48b5d45e77806e11
Michael Dm <mhs_86@mail.ru>
2015-04-09 01:08:53 +0300

    week6: blackjack: assigment complete

--- week_6/blackJack.py
+++ week_6/blackJack.py
@@ -6,6 +6,7 @@ import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
 
 # load card sprite - 936x384 - source: jfitz.com
+CARD_POSITION = ((50, 400), (50, 150))
 CARD_SIZE = (72, 96)
 CARD_CENTER = (36, 48)
 card_images = simplegui.load_image("http://storage.googleapis.com/codeskulptor-assets/cards_jfitz.png")
@@ -16,7 +17,8 @@ card_back = simplegui.load_image("http://storage.googleapis.com/codeskulptor-ass
 
 # initialize some useful global variables
 in_play = False
-outcome = ""
+dealer_message = ""
+player_message = ""
 score = 0
 
 # define globals for cards
@@ -50,6 +52,7 @@ class Card:
                     CARD_CENTER[1] + CARD_SIZE[1] * SUITS.index(self.suit))
         canvas.draw_image(card_images, card_loc, CARD_SIZE, [pos[0] + CARD_CENTER[0], pos[1] + CARD_CENTER[1]], CARD_SIZE)
 
+
 # define hand class
 class Hand:
     def __init__(self):
@@ -80,9 +83,13 @@ class Hand:
                 return self.hand_value
 
     def draw(self, canvas, pos):
-        pass  # draw a hand on the canvas, use the draw method for cards
+        offset = 0
+        for card in self.cards:
+            card.draw(canvas, (pos[0] + offset, pos[1]))
+            offset += 100
 
-# define deck class 
+
+# define deck class
 class Deck:
     def __init__(self):
         self.cards = list()
@@ -102,10 +109,14 @@ class Deck:
             res += " " + str(card)
         return res
 
+
 # define event handlers for buttons
 def deal():
-    global outcome, in_play, deck, player, dealer
+    global dealer_message, in_play, deck, player, dealer, score, player_message
+    if in_play:
+        score -= 1
 
+    dealer_message = ""
     deck = Deck()
     deck.shuffle()
 
@@ -117,50 +128,75 @@ def deal():
     player.add_card(deck.deal_card())
     dealer.add_card(deck.deal_card())
 
-    print "PLAYER " + str(player) + " value: " + str(player.get_value())
-    print "DEALER " + str(dealer) + " value: " + str(dealer.get_value())
+    # print "PLAYER " + str(player) + " value: " + str(player.get_value())
+    #print "DEALER " + str(dealer) + " value: " + str(dealer.get_value())
 
     in_play = True
+    player_message = "Hit or stand?"
+
 
 def hit():
-    global player, deck, in_play
+    global player, deck, in_play, score, dealer_message, player_message
 
     if in_play:
         player.add_card(deck.deal_card())
-        print "PLAYER " + str(player) + " value: " + str(player.get_value())
+        # print "PLAYER " + str(player) + " value: " + str(player.get_value())
         if player.get_value() > 21:
-            print "You have busted"
+            dealer_message = "You have busted"
             in_play = False
+            score -= 1
+            player_message = "New deal?"
             # if busted, assign a message to outcome, update in_play and score
 
+
 def stand():
-    global in_play, dealer, player
+    global in_play, dealer, player, score, dealer_message, player_message
 
     if not in_play:
-        print "Player have busted"
+        return
     else:
         while dealer.get_value() < 17:
             dealer.add_card(deck.deal_card())
-            print "DEALER " + str(dealer) + " value: " + str(dealer.get_value())
+            # print "DEALER " + str(dealer) + " value: " + str(dealer.get_value())
         if dealer.get_value() > 21:
-            print "Dealer have busted"
+            dealer_message = "Dealer have busted"
+            score += 1
+            in_play = False
+            player_message = "New deal?"
+            return
 
         if player.get_value() <= dealer.get_value():
-            print "Dealer wins"
+            dealer_message = "Dealer wins"
+            score -= 1
         else:
-            print "Player wins"
+            dealer_message = "Player wins"
+            score += 1
 
-            # if hand is in play, repeatedly hit dealer until his hand has value 17 or more
+    in_play = False
+    player_message = "New deal?"
 
-            # assign a message to outcome, update in_play and score
 
-# draw handler    
+# draw handler
 def draw(canvas):
-    # test to make sure that card.draw works, replace with your code below
+    global player, dealer, score
 
-    card = Card("S", "A")
-    card.draw(canvas, [300, 300])
+    canvas.draw_text("Blackjack", (180, 40), 50, "Red")
+    canvas.draw_text("Scores " + str(score), (450, 315), 25, "Yellow")
+    canvas.draw_text("Player", (0, CARD_POSITION[0][1] - 15), 50, "Black")
+    canvas.draw_text("Dealer", (0, CARD_POSITION[1][1] - 15), 50, "Black")
 
+    canvas.draw_text(dealer_message, (150, CARD_POSITION[1][1] - 15), 50, "Red")
+    canvas.draw_text(player_message, (150, CARD_POSITION[0][1] - 15), 50, "Red")
+
+    player.draw(canvas, CARD_POSITION[0])
+    dealer.draw(canvas, CARD_POSITION[1])
+
+    if in_play:
+        canvas.draw_image(card_back, (CARD_CENTER[0], CARD_CENTER[1]), CARD_BACK_SIZE,
+                          (CARD_POSITION[1][0] + CARD_BACK_CENTER[0], CARD_POSITION[1][1] + CARD_BACK_CENTER[1]), CARD_BACK_SIZE)
+
+
+deal()
 # initialization frame
 frame = simplegui.create_frame("Blackjack", 600, 600)
 frame.set_canvas_background("Green")
@@ -173,8 +209,4 @@ frame.set_draw_handler(draw)
 
 
 # get things rolling
-deal()
 frame.start()
-
-
-# remember to review the gradic rubric
