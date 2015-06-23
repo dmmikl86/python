7fb9e4f7fb70bbbc5730477cbb5ab114724d1480
Michael Dmytriaha <mdmytiaha@playtika.com.ua>
2015-06-23 16:54:40 +0300

    PoC: week4: assignment complete

--- Principles of Computing/week4/yahtzee.py
+++ Principles of Computing/week4/yahtzee.py
@@ -5,6 +5,8 @@ Simplifications:  only allow discard and roll, only score against upper level
 
 # Used to increase the timeout, if necessary
 import SimpleGUICS2Pygame.codeskulptor as codeskulptor
+#import codeskulptor
+
 codeskulptor.set_timeout(20)
 
 def gen_all_sequences(outcomes, length):
@@ -12,7 +14,7 @@ def gen_all_sequences(outcomes, length):
     Iterative function that enumerates the set of all sequences of
     outcomes of given length.
     """
-    
+
     answer_set = set([()])
     for dummy_idx in range(length):
         temp_set = set()
@@ -24,7 +26,6 @@ def gen_all_sequences(outcomes, length):
         answer_set = temp_set
     return answer_set
 
-
 def score(hand):
     """
     Compute the maximal score for a Yahtzee hand according to the
@@ -34,9 +35,12 @@ def score(hand):
 
     Returns an integer score 
     """
+    max_value = []
     item_count = [hand.count(item) for item in hand]
-    return item_count
-
+    for idx in range(len(hand)):
+        max_value.append(hand[idx] * item_count[idx])
+    # print "hand to evaluate: " + str(hand) + " hand value:" + str(max(max_value))
+    return max(max_value)
 
 def expected_value(held_dice, num_die_sides, num_free_dice):
     """
@@ -49,8 +53,11 @@ def expected_value(held_dice, num_die_sides, num_free_dice):
 
     Returns a floating point expected value
     """
-    return 0.0
-
+    value = 0.0
+    sequences = gen_all_sequences(range(1, num_die_sides + 1), num_free_dice)
+    for seq in sequences:
+        value += score(held_dice + seq)
+    return float(value / len(sequences))
 
 def gen_all_holds(hand):
     """
@@ -60,9 +67,12 @@ def gen_all_holds(hand):
 
     Returns a set of tuples, where each tuple is dice to hold
     """
-    return set([()])
+    new_hand = [()]
+    for item in hand:
+        for subset in new_hand:
+            new_hand = new_hand + [tuple(subset) + (item, )]
 
-
+    return set(new_hand)
 
 def strategy(hand, num_die_sides):
     """
@@ -75,21 +85,27 @@ def strategy(hand, num_die_sides):
     Returns a tuple where the first element is the expected score and
     the second element is a tuple of the dice to hold
     """
-    return (0.0, ())
+    result = (0.0, ())
+    current_value = 0.0
 
+    for item in gen_all_holds(hand):
+        value = expected_value(item, num_die_sides, len(hand) - len(item))
+        if value > current_value:
+            current_value = value
+            result = (current_value, item)
+
+    return result
 
 def run_example():
     """
     Compute the dice to hold and expected score for an example hand
     """
     num_die_sides = 6
-    hand = (1, 1, 1, 5, 6)
+    hand = (6,6,2,4,4)
     hand_score, hold = strategy(hand, num_die_sides)
     print "Best strategy for hand", hand, "is to hold", hold, "with expected score", hand_score
-    
-    
-run_example()
 
+run_example()
 
 #import poc_holds_testsuite
 #poc_holds_testsuite.run_suite(gen_all_holds)
