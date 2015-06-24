8f5830ce9bc83ed781183fa21fa449d955ca0a3a
Michael Dmytriaha <mdmytiaha@playtika.com.ua>
2015-06-24 12:15:40 +0300

    PoC: week5: working on assignment

--- Principles of Computing/week5/poc_greedy_solution.py
+++ Principles of Computing/week5/poc_greedy_solution.py
@@ -2,7 +2,8 @@
 Simulator for greedy boss scenario
 """
 
-import matplotlib.pyplot as simpleplot
+import SimpleGUICS2Pygame.simpleplot as simpleplot
+import matplotlib.pyplot as plot
 import math
 import SimpleGUICS2Pygame.codeskulptor as codeskulptor
 codeskulptor.set_timeout(20)
@@ -72,10 +73,14 @@ def run_simulations():
     inc_500 = greedy_boss(days, 500, plot_type)
     inc_1000 = greedy_boss(days, 1000, plot_type)
     inc_2000 = greedy_boss(days, 2000, plot_type)
-    simpleplot.plot([x[0] for x in inc_0], [x[1] for x in inc_0])
-    simpleplot.plot([x[0] for x in inc_500], [x[1] for x in inc_500])
-    simpleplot.plot([x[0] for x in inc_1000], [x[1] for x in inc_1000])
-    simpleplot.plot([x[0] for x in inc_2000], [x[1] for x in inc_2000])
+    simpleplot.plot_lines("Greedy boss", 600, 600, "days", "total earnings",
+                          [inc_0, inc_500, inc_1000, inc_2000], False,
+                         ["Bribe increment = 0", "Bribe increment = 500",
+                          "Bribe increment = 1000", "Bribe increment = 2000"])
+    # simpleplot.plot([x[0] for x in inc_0], [x[1] for x in inc_0])
+    # simpleplot.plot([x[0] for x in inc_500], [x[1] for x in inc_500])
+    # simpleplot.plot([x[0] for x in inc_1000], [x[1] for x in inc_1000])
+    # simpleplot.plot([x[0] for x in inc_2000], [x[1] for x in inc_2000])
 
 run_simulations()
 
@@ -84,4 +89,4 @@ print greedy_boss(35, 100)
 
 print greedy_boss(35, 0)
 # should print [(0, 0), (10, 1000), (15, 2000), (19, 3200), (21, 4000), (23, 5000), (25, 6200), (27, 7600), (28, 8400), (29, 9300), (30, 10300), (31, 11400), (32, 12600), (33, 13900), (34, 15300), (34, 15300), (35, 16900), (36, 18600)]
-simpleplot.show()
+plot.show()
